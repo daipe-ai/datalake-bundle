17f23b36088d1740f53ad2373766e4add565d915
Jiri Koutny <jirkakoutny@gmail.com>
2020-02-27 06:40:04 +0100

    Added a way how to run commands in bundle

--- src/datalakebundle/DataLakeBundleTest.py
+++ src/datalakebundle/DataLakeBundleTest.py
@@ -1,33 +1,11 @@
 import unittest
-from consolebundle.ConsoleBundle import ConsoleBundle
 from injecta.testing.servicesTester import testServices
-from injecta.config.YamlConfigReader import YamlConfigReader
-from injecta.package.pathResolver import resolvePath
-from typing import List
-from pyfony.PyfonyBundle import PyfonyBundle
-from pyfony.kernel.BaseKernel import BaseKernel
-from pyfonybundles.Bundle import Bundle
-from datalakebundle.DataLakeBundle import DataLakeBundle
+from datalakebundle.containerInit import initContainer
 
 class DataLakeBundleTest(unittest.TestCase):
 
     def test_init(self):
-        class Kernel(BaseKernel):
-
-            def _registerBundles(self) -> List[Bundle]:
-                return [
-                    PyfonyBundle(),
-                    ConsoleBundle(),
-                    DataLakeBundle()
-                ]
-
-        kernel = Kernel(
-            'test',
-            resolvePath('datalakebundle') + '/DataLakeBundleTest',
-            YamlConfigReader()
-        )
-
-        container = kernel.initContainer()
+        container = initContainer('test')
 
         testServices(container)
 
---
+++ src/datalakebundle/containerInit.py
@@ -0,0 +1,27 @@
+from consolebundle.ConsoleBundle import ConsoleBundle
+from injecta.config.YamlConfigReader import YamlConfigReader
+from injecta.container.ContainerInterface import ContainerInterface
+from injecta.package.pathResolver import resolvePath
+from typing import List
+from pyfony.PyfonyBundle import PyfonyBundle
+from pyfony.kernel.BaseKernel import BaseKernel
+from pyfonybundles.Bundle import Bundle
+from datalakebundle.DataLakeBundle import DataLakeBundle
+
+def initContainer(appEnv) -> ContainerInterface:
+    class Kernel(BaseKernel):
+
+        def _registerBundles(self) -> List[Bundle]:
+            return [
+                PyfonyBundle(),
+                ConsoleBundle(),
+                DataLakeBundle()
+            ]
+
+    kernel = Kernel(
+        appEnv,
+        resolvePath('datalakebundle') + '/_config',
+        YamlConfigReader()
+    )
+
+    return kernel.initContainer()
