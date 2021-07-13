86d2c96cb17a2326bfbd1ab9a98180abbc3e9160
Lukáš Langr <82815273+ds-lukaslangr@users.noreply.github.com>
2021-07-13 11:06:40 +0200

    Fix transformation to work with Pandas Dataframe (#5)

--- src/datalakebundle/notebook/decorator/transformation.py
+++ src/datalakebundle/notebook/decorator/transformation.py
@@ -1,4 +1,6 @@
 from daipecore.decorator.DecoratedDecorator import DecoratedDecorator
+from pyspark.sql import DataFrame
+
 from datalakebundle.notebook.decorator.DataFrameReturningDecorator import DataFrameReturningDecorator
 from datalakebundle.notebook.decorator.DuplicateColumnsChecker import DuplicateColumnsChecker
 from injecta.container.ContainerInterface import ContainerInterface
@@ -14,7 +16,8 @@ class transformation(DataFrameReturningDecorator):  # noqa: N801
 
     def after_execution(self, container: ContainerInterface):
         if (
-            self._result
+            self._result is not None
+            and isinstance(self.result, DataFrame)
             and self._check_duplicate_columns
             and container.get_parameters().datalakebundle.notebook.duplicate_columns_check.enabled is True
         ):
@@ -27,6 +30,6 @@ class transformation(DataFrameReturningDecorator):  # noqa: N801
 
         self._set_global_dataframe()
 
-        if self._result and self._display and container.get_parameters().datalakebundle.notebook.display.enabled is True:
+        if self._result is not None and self._display and container.get_parameters().datalakebundle.notebook.display.enabled is True:
             data_frame_show_method: DataFrameShowMethodInterface = container.get("pysparkbundle.dataframe.show_method")
             data_frame_show_method.show(self._result)
